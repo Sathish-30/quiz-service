cc169d7a6f6e7706f552fa388b2b25cdaee31a57
Sathish-39 <sathish.amsaravanan@gmail.com>
2023-07-18 05:42:43 +0530

    Added the required files from quiz Application

---
+++ src/main/java/com/learning/quiz_service/model/Response.java
@@ -0,0 +1,11 @@
+package com.learning.quiz_service.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+}
