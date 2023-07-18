a885e9668f8fbb0dd72343e700cd79cb78549fe5
Sathish-39 <sathish.amsaravanan@gmail.com>
2023-07-18 06:08:23 +0530

    Added the DTO file for getting data and adding the Questions , Added the README.md with some contents

---
+++ README.md
@@ -0,0 +1,5 @@
+### Quiz_Service
+
+> Quiz Service is the 2nd microservice build after the question microservice , In which added the Controller , Service and the repositry packages.
+> It requires a Rest template to get response from the question_service so we use feign client and eureka server for easy access of data without hard coding the 
+> IP address and the port number
--- src/main/java/com/learning/quiz_service/controller/QuizController.java
+++ src/main/java/com/learning/quiz_service/controller/QuizController.java
@@ -1,9 +1,11 @@
 package com.learning.quiz_service.controller;
 
 
-import com.project.quiz_application.model.QuestionWrapper;
-import com.project.quiz_application.model.Response;
-import com.project.quiz_application.service.QuizService;
+
+import com.learning.quiz_service.dto.QuizDTO;
+import com.learning.quiz_service.model.QuestionWrapper;
+import com.learning.quiz_service.model.Response;
+import com.learning.quiz_service.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +19,8 @@ public class QuizController {
     @Autowired
     QuizService quizService;
     @PostMapping("create")
-    public ResponseEntity<String> createQuiz(@RequestParam String category , @RequestParam int count , @RequestParam String title){
-        return quizService.createQuiz(category , count , title );
+    public ResponseEntity<String> createQuiz(@RequestBody QuizDTO quizDTO){
+        return quizService.createQuiz(quizDTO.getCategoryName() , quizDTO.getNumQuestions() , quizDTO.getTitle() );
     }
 
     @GetMapping("get/{id}")
